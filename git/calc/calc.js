f3eefe3494341a330dafc9865036e207b6397cb1
Dzmitry-Snegirev <snegirev.dzmitry@gmail.com>
2022-01-30 12:17:54 +0300

    calc

--- git/calc/calc.js
+++ git/calc/calc.js
@@ -2,27 +2,47 @@
 var strTop = prompt("Введите строку для расчёта");
 function calc(str) {
 	var result = 0;
-
-
+	var znak = false;
+	if (str.charAt(0) === "-") {
+		var strClear = str.slice(1);
+		str = str.replace(str, strClear);
+		znak = true;
+	}
 	if (str.indexOf('(') != -1) {
 		var a = str.indexOf('(');
 		var b = str.indexOf(')');
 		var strbot = calc(str.slice(a + 1, b));
+		if (strbot.charAt(0) === "-") {
+			var strClear = strbot.slice(1);
+			strbot = strbot.replace(strbot, strClear);
+			znak = true;
+		}
 		str = str.replace(str.slice(a, b + 1), strbot);
 	}
+
 	if (isNaN(str)) {
 		if (str.indexOf('+') != -1) {
 			str = str.split('+');
 			result = calc(str[0]);
 			for (var i = 1; i < str.length; i++) {
-				result = result + calc(str[i]);
+				if (znak === true) {
+					result = result - calc(str[i]);
+				}
+				else {
+					result = result + calc(str[i]);
+				}
 			}
 		}
 		else if (str.indexOf('-') != -1) {
 			str = str.split('-');
 			result = calc(str[0]);
 			for (var i = 1; i < str.length; i++) {
-				result = result - calc(str[i]);
+				if (znak === true) {
+					result = result + calc(str[i]);
+				}
+				else {
+					result = result - calc(str[i]);
+				}
 			}
 		}
 		else if (str.indexOf('*') != -1) {
@@ -41,6 +61,9 @@ function calc(str) {
 		}
 	}
 	else (result = parseFloat(str));
+	if (znak === true) {
+		result = "-" + result;
+	}
 	return result;
 }
 console.log(calc(strTop));
