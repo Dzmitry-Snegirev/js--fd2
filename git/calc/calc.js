e1fe462f77ac48294a039c97da6ddde282a81a60
Dzmitry-Snegirev <snegirev.dzmitry@gmail.com>
2022-02-15 14:43:26 +0300

    fix calc

--- git/calc/calc.js
+++ git/calc/calc.js
@@ -1,96 +1,200 @@
 "use strict"
 var strTop = prompt("Введите строку для расчёта");
 function calc(str) {
+	var srtY = "";
 	var result = 0;
 	var znak = false;
 	var turn = false;
 
-	if (str.charAt(0) === "-") {
-		var strClear = str.slice(1);
-		str = str.replace(str, strClear);
-		znak = true;
-	}
-
-	if (str.indexOf('(') != -1) {
-		var a = str.indexOf('(');
-		var b = str.indexOf(')');
-		var strbot = calc(str.slice(a + 1, b));
-		for (var i = 0; i < strbot.length; i++) {
-			if (strbot[0] === "-") {
-				var strClear = strbot.slice(1);
-				strbot = strbot.replace(strbot, strClear);
-				znak = true;
-			}
+	for (var i = 0; i < str.length; i++) {
+		if (str.indexOf('(') != -1) {
+			var m = str.indexOf('(');
+			var n = str.indexOf(')');
+			var strbot = calc(str.slice(m + 1, n));
+			var str = str.replace(str.slice(m, n + 1), strbot);
 		}
-		str = str.replace(str.slice(a, b + 1), strbot);
 	}
-
-	if (str.indexOf('+') != -1) {
-		if (str.indexOf('-') != -1) {
-			var m = str.indexOf('-');
-			str = str.slice(0, m) + str.slice(m + 1, str.length);
-			znak = true;
-			turn = false;
-		}
-		str = str.split('+');
-		result = calc(str[0]);
-		for (var i = 1; i < str.length; i++) {
-			if (znak === true) {
-				result = result - calc(str[i]);
-				znak = false;
-			}
-			else {
-				result = result + calc(str[i]);
-			}
+	if (str.indexOf('-') != -1) {
+		var a = str.indexOf('-');
+		if (str.charAt(a + 1) == "-") {
+			str = str.replace(str.slice(a, a + 2), "+");
 		}
 	}
 
-	else if (str.indexOf('*') != -1) {
-		if (znak === true && str.indexOf('-') != -1) {
-			var p = str.indexOf('-');
-			str = str.slice(0, p) + str.slice(p + 1, str.length);
-			znak = false;
-		}
-		str = str.split('*');
-		result = calc(str[0]);
-		for (var i = 1; i < str.length; i++) {
-			result = result * calc(str[i]);
-		}
-	}
-	else if (str.indexOf('/') != -1) {
-		if (znak === true && str.indexOf('-') != -1) {
-			var r = str.indexOf('-');
-			str = str.slice(0, r) + str.slice(r + 1, str.length);
-			znak = false;
-		}
-		str = str.split('/');
-		result = calc(str[0]);
-		for (var i = 1; i < str.length; i++) {
-			result = result / calc(str[i]);
-		}
-	}
-	else if (str.indexOf('-') != -1) {
-		var f = str.indexOf('-');
+	if (isNaN(str)) {
 		for (var i = 0; i < str.length; i++) {
-			if (str[f + 1] === "-") {
-				str = str.slice(0, f) + str.slice(f + 1, str.length);
+			if (str[0] === "-") {
 				znak = true;
-				turn = true;
 			}
 		}
-		str = str.split('-');
-		result = calc(str[0]);
-		for (var i = 1; i < str.length; i++) {
+		if (str.indexOf('*') != -1) {
 			if (znak === true) {
-				result = result + calc(str[i]);
-				znak === false;
+				var str = str.slice(1);
+				var a = str.indexOf('*');
+				if (str.charAt(a + 1) == "-") {
+					str = str.replace(str.slice(a, a + 2), "*");
+					str = str.split('*');
+					result = calc(str[0]);
+					for (var i = 1; i < str.length; i++) {
+						result = result * calc(str[i]);
+					}
+					turn = true;
+				}
+				else {
+					str = str.split('*');
+					result = calc(str[0]);
+					for (var i = 1; i < str.length; i++) {
+						result = calc(str[i]) * result;
+					}
+				}
+
 			}
-			else {
-				result = result - calc(str[i]);
+			if (znak === false) {
+				var a = str.indexOf('*');
+				if (str.charAt(a + 1) == "-") {
+					str = str.replace(str.slice(a, a + 2), "*");
+					str = str.split('*');
+					result = calc(str[0]);
+					for (var i = 1; i < str.length; i++) {
+						result = result * calc(str[i]);
+					}
+					znak = true;
+				}
+				else {
+					str = str.split('*');
+					result = calc(str[0]);
+					for (var i = 1; i < str.length; i++) {
+						result = calc(str[i]) * result;
+					}
+				}
+			}
+		}
+		else if (str.indexOf('/') != -1) {
+			if (znak === true) {
+				var str = str.slice(1);
+				var a = str.indexOf('/');
+				if (str.charAt(a + 1) == "-") {
+					str = str.replace(str.slice(a, a + 2), "/");
+					str = str.split('/');
+					result = calc(str[0]);
+					for (var i = 1; i < str.length; i++) {
+						result = result / calc(str[i]);
+					}
+					turn = true;
+				}
+				else {
+					str = str.split('/');
+					result = calc(str[0]);
+					for (var i = 1; i < str.length; i++) {
+						result = result / calc(str[i]);
+					}
+				}
+
+			}
+			if (znak === false) {
+				var a = str.indexOf('/');
+				if (str.charAt(a + 1) == "-") {
+					str = str.replace(str.slice(a, a + 2), "/");
+					str = str.split('/');
+					result = calc(str[0]);
+					for (var i = 1; i < str.length; i++) {
+						result = result / calc(str[i]);
+					}
+					znak = true;
+				}
+				else {
+					str = str.split('/');
+					result = calc(str[0]);
+					for (var i = 1; i < str.length; i++) {
+						result = result / calc(str[i]);
+					}
+				}
+			}
+		}
+		else if (str.indexOf('+') != -1) {
+			if (znak === true) {
+				var str = str.slice(1);
+				if (str.indexOf('-') != -1) {
+					var b = str.indexOf('+');
+					if (str.charAt(b + 1) == "-") {
+						str = str.replace(str.slice(b, b + 2), "+");
+						str = str.split('+');
+						for (var i = 0; i < str.length; i++) {
+							result = result + calc(str[i]);
+						}
+					}
+				}
+				else {
+					str = str.split('+');
+					result = calc(str[0]);
+					for (var i = 1; i < str.length; i++) {
+						result = calc(str[i]) - result;
+						turn = true;
+					}
+				}
+			}
+			if (znak === false) {
+				var a = str.indexOf('+');
+				if (str.charAt(a + 1) == "-") {
+					str = str.replace(str.slice(a, a + 2), "-");
+					str = str.split('-');
+					result = calc(str[0]);
+					for (var i = 1; i < str.length; i++) {
+						result = result - calc(str[i]);
+					}
+				}
+				else {
+					str = str.split('+');
+					result = calc(str[0]);
+					for (var i = 1; i < str.length; i++) {
+						result = result + calc(str[i]);
+					}
+				}
+			}
+		}
+		else if (str.indexOf('-') != -1) {
+			if (znak === true) {
+				var str = str.slice(1);
+				if (str.indexOf('-') != -1) {
+					var c = str.indexOf('-');
+					if (str.charAt(c + 1) == "-") {
+						str = str.replace(str.slice(c, c + 2), "-");
+						str = str.split('-');
+						result = calc(str[0]);
+						for (var i = 1; i < str.length; i++) {
+							result = calc(str[i]) - result;
+							turn = true;
+						}
+					}
+					else {
+						str = str.split('-');
+						result = calc(str[0]);
+						for (var i = 1; i < str.length; i++) {
+							result = result + calc(str[i]);
+						}
+					}
+				}
+			}
+			if (znak === false) {
+				var a = str.indexOf('-');
+				if (str.charAt(a + 1) == "-") {
+					str = str.replace(str.slice(a, a + 2), "+");
+					str = str.split('+');
+					result = calc(str[0]);
+					for (var i = 1; i < str.length; i++) {
+						result = result + calc(str[i]);
+					}
+				}
+				else {
+					str = str.split('-');
+					result = calc(str[0]);
+					for (var i = 1; i < str.length; i++) {
+						result = result - calc(str[i]);
+					}
+				}
 			}
 		}
 	}
-
 	else (result = parseFloat(str));
 	if (znak === true && result !== 0 && turn === false) {
 		result = "-" + result;
